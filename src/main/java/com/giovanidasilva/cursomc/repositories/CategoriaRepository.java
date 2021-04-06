2376d4082fae22a9daf753f9878d8aa6fab02b72
GiovaniDaSilva <giovani-vani@hotmail.com>
2021-04-06 20:58:21 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/giovanidasilva/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.giovanidasilva.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.giovanidasilva.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
