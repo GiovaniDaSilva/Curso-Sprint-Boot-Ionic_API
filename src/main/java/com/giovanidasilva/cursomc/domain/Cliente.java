a8573a07e9f7088656d680fab0b77cf538a5e6ea
GiovaniDaSilva <giovani-vani@hotmail.com>
2021-04-10 21:17:02 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/giovanidasilva/cursomc/domain/Cliente.java
+++ src/main/java/com/giovanidasilva/cursomc/domain/Cliente.java
@@ -39,6 +39,10 @@ public class Cliente  implements Serializable {
 	//Set não permite repetição
 	private Set<String> telefones = new HashSet<>();
 	
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -108,6 +112,15 @@ public class Cliente  implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -132,6 +145,7 @@ public class Cliente  implements Serializable {
 			return false;
 		return true;
 	}
-	
+
+
 	
 }
