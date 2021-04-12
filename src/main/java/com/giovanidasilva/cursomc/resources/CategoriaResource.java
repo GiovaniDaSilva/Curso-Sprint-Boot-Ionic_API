ac2812fb0fe1c3df66e6390a2bda5f1d677a2549
GiovaniDaSilva <giovani-vani@hotmail.com>
2021-04-12 20:36:06 -0300

    Listando todas categorias com DTO

--- src/main/java/com/giovanidasilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/giovanidasilva/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,10 @@
 package com.giovanidasilva.cursomc.resources;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.giovanidasilva.cursomc.domain.Categoria;
+import com.giovanidasilva.cursomc.dto.CategoriaDTO;
 import com.giovanidasilva.cursomc.services.CategoriaService;
 
 @RestController
@@ -50,5 +55,16 @@ public class CategoriaResource {
 		service.delete(id);		
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();		
+		
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);		
+	}
+	
 	
 }
--- src/main/java/com/giovanidasilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/giovanidasilva/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.giovanidasilva.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,8 +43,11 @@ public class CategoriaService {
 			repo.deleteById(id);	
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegratyException("Não é possível excluir uma categoria que possui produtos.");
-		}
-		
+		}		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
